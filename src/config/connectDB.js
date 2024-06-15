3e3536f505268269e9ecd8ffb1c023b8fb1ba9d5
shadow7292 <72336218+shadow7292@users.noreply.github.com>
2024-06-15 18:08:22 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
     host: '142.171.153.18',
-    user: 'a7669371_p4radoxe',
+    user: 'a7669371_biglottery',
     password: '729288@Rishu$',
-    database: 'a7669371_paisaclub',
+    database: 'a7669371_biglottery',
 });
 
 export default connection;
