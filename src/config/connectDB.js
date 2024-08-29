e174473eae35a63f6acac1909796650815605233
Aryanmalaysia <aryanmalikmalaysia@gmail.com>
2024-08-29 07:43:02 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '142.171.153.18',
-    user: 'a7669371_biglottery',
+    host: '85.195.73.202',
+    user: 'win1209_Win100user',
     password: '729288@Rishu$',
-    database: 'a7669371_biglottery',
+    database: 'win1209_win100',
 });
 
 export default connection;
